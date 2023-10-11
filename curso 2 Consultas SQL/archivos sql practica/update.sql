adc9010c5f6e62d0bfa611da638eda4d7d337326
Iñaki Cruz Elizondo Rios <116087814+CruzElizorios@users.noreply.github.com>
2023-10-11 15:54:23 -0300

    learn-update-insert

---
+++ curso 2 Consultas SQL/archivos sql practica/update.sql
@@ -0,0 +1,5 @@
+SELECT * FROM tabla_de_productos;
+
+UPDATE tabla_de_productos SET PRECIO_DE_LISTA = 35 WHERE CODIGO_DEL_PRODUCTO = '1022450';
+UPDATE tabla_de_productos SET NOMBRE_DEL_PRODUCTO = 'Refrescante', TAMANO = '1 Litro' , ENVASE = 'Botella PET' 
+WHERE CODIGO_DEL_PRODUCTO = '1000889';
